74edc334f9463ec2fb4f7ed4bdab683c0c7343ee
yu-podong <yu01gun@naver.com>
2021-05-13 01:43:35 +0900

    Re-create License Activities for Intent

---
+++ app/src/main/java/com/yupodong/lins/License/LicenseActivity.java
@@ -0,0 +1,13 @@
+package com.yupodong.lins.License;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LicenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
