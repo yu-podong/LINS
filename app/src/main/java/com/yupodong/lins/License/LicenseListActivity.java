c48426e571bee90d0fbd319d2313aeef67498342
Jeon-Ahhyeon <dkgus4012@naver.com>
2021-05-13 21:21:49 +0900

    xml 파일 기본 배경버튼 설정

--- app/src/main/java/com/yupodong/lins/License/LicenseListActivity.java
+++ app/src/main/java/com/yupodong/lins/License/LicenseListActivity.java
@@ -3,11 +3,75 @@ package com.yupodong.lins.License;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.yupodong.lins.R;
 
 public class LicenseListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license_list);
+
+
+        ImageButton backBtn = (ImageButton)findViewById(R.id.backBtn);
+        ImageButton myBtn = (ImageButton)findViewById(R.id.myBtn);
+        ImageButton licenBtn = (ImageButton)findViewById(R.id.licenBtn);
+        ImageButton calenBtn = (ImageButton)findViewById(R.id.calenBtn);
+        ImageButton homeBtn = (ImageButton)findViewById(R.id.homeBtn);
+        ImageButton commBtn = (ImageButton)findViewById(R.id.commBtn);
+        ImageButton chalBtn = (ImageButton)findViewById(R.id.chalBtn);
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {  //license 페이지로 이동
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_license_select);
+            }
+        });
+
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);  //수정페이지 없어서 일단 메인으로 이동
+            }
+        });
+
+        licenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_license_list);
+            }
+        });
+
+        calenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_sche);
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {  //메인페이지 버튼
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+        commBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_commu);
+            }
+        });
+
+        chalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_commu);  //문의사항 페이지 없음 새로 생성해야 함  일단 커뮤로 이동
+            }
+        });
     }
 }
--- app/src/main/java/com/yupodong/lins/Scheduler/SchedulerActivity.java
+++ app/src/main/java/com/yupodong/lins/Scheduler/SchedulerActivity.java
@@ -1,13 +1,78 @@
 package com.yupodong.lins.Scheduler;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.yupodong.lins.R;
 
 public class SchedulerActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sche);
+
+
+        ImageButton backBtn = (ImageButton)findViewById(R.id.backBtn);
+        ImageButton myBtn = (ImageButton)findViewById(R.id.myBtn);
+        ImageButton licenBtn = (ImageButton)findViewById(R.id.licenBtn);
+        ImageButton calenBtn = (ImageButton)findViewById(R.id.calenBtn);
+        ImageButton homeBtn = (ImageButton)findViewById(R.id.homeBtn);
+        ImageButton commBtn = (ImageButton)findViewById(R.id.commBtn);
+        ImageButton chalBtn = (ImageButton)findViewById(R.id.chalBtn);
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {  //뒤로 돌아가기
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+        licenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_license_list);
+            }
+        });
+
+        calenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_sche);
+            }
+        });
+
+        homeBtn.setOnClickListener(new View.OnClickListener() {  //메인페이지 버튼
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main);
+            }
+        });
+
+        commBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_commu);
+            }
+        });
+
+        chalBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_commu);  //문의사항 페이지 없음 새로 생성해야 함
+            }
+        });
     }
+
 }
