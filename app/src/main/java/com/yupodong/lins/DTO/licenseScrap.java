205ca56cc93c99954b5525eaca1d53e741fded67
yu-podong <yu01gun@naver.com>
2021-05-25 01:15:11 +0900

    Modify attribute

--- app/src/main/java/com/yupodong/lins/DTO/licenseScrap.java
+++ app/src/main/java/com/yupodong/lins/DTO/licenseScrap.java
@@ -1,42 +1,38 @@
 package com.yupodong.lins.DTO;
 
 public class licenseScrap {
-    private Integer scrapID;
     private Integer licenseID;
     private String licenseName;
+    private String nickName;
 
     // 생성자
     public licenseScrap() {
-        this.scrapID = 0;
         this.licenseID = 0;
         this.licenseName = "";
+        this.nickName = "";
     }
-    public licenseScrap(Integer scrapID, Integer licenseID, String licenseName) {
-        this.scrapID = scrapID;
+    public licenseScrap( Integer licenseID, String licenseName, String nickName) {
         this.licenseID = licenseID;
         this.licenseName = licenseName;
+        this.nickName = nickName;
     }
 
     // 접근자함수
     // Set함수
-    public void setScrapID(Integer scrapID) {
-        this.scrapID = scrapID;
-    }
     public void setLicenseID(Integer licenseID) {
         this.licenseID = licenseID;
     }
     public void setLicenseName(String licenseName) {
         this.licenseName = licenseName;
     }
+    public void setNickName(String nickName) { this.nickName = nickName; }
 
     // Get함수
-    public Integer getScrapID() {
-        return this.scrapID;
-    }
     public Integer getLicenseID() {
         return this.licenseID;
     }
     public String getLicenseName() {
         return this.licenseName;
     }
+    public String getNickName() { return this.nickName; }
 }
