2d2ba0a7db612670be1682989b93b918fa394f84
yu-podong <yu01gun@naver.com>
2021-05-28 11:29:18 +0900

    test

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/yupodong/lins/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.yupodong.lins.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.yupodong.lins.test";
+  public static final String BUILD_TYPE = "debug";
+}
